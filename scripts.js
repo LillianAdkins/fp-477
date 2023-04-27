7cdf7fd64c37cca4a4087ee081728c669beb0c36
LillianAdkins <123781294+LillianAdkins@users.noreply.github.com>
2023-04-26 20:38:49 -0700

    boxes and varibales

--- scripts.js
+++ scripts.js
@@ -1,4 +1,62 @@
 $(document).ready(function(){
-  $("h1").click(function(){
-console.log("hello world")});
+
+  //variables
+  let game = 0;
+  let gameRed = 0;
+  let gameBlue = 0;
+  let draw = 0;
+  let gameStart = true;
+  let startingMark = "red";
+  let turns = 0;
+
+  //boxes
+  //box a
+  let cella1 = $("#a1").text();
+  let cella2 = $("#a2").text();
+  let cella3 = $("#a3").text();
+  let cella4 = $("#a4").text();
+  let cella5 = $("#a5").text();
+  let cella6 = $("#a6").text();
+  let cella7 = $("#a7").text();
+
+  let cellb1 = $("#b1").text();
+  let cellb2 = $("#b2").text();
+  let cellb3 = $("#b3").text();
+  let cellb4 = $("#b4").text();
+  let cellb5 = $("#b5").text();
+  let cellb6 = $("#b6").text();
+  let cellb7 = $("#b7").text();
+
+  let cellc1 = $("#c1").text();
+  let cellc2 = $("#c2").text();
+  let cellc3 = $("#c3").text();
+  let cellc4 = $("#c4").text();
+  let cellc5 = $("#c5").text();
+  let cellc6 = $("#c6").text();
+  let cellc7 = $("#c7").text();  
+  
+  let celld1 = $("#d1").text();
+  let celld2 = $("#d2").text();
+  let celld3 = $("#d3").text();
+  let celld4 = $("#d4").text();
+  let celld5 = $("#d5").text();
+  let celld6 = $("#d6").text();
+  let celld7 = $("#d7").text();
+
+  let celle1 = $("#e1").text();
+  let celle2 = $("#e2").text();
+  let celle3 = $("#e3").text();
+  let celle4 = $("#e4").text();
+  let celle5 = $("#e5").text();
+  let celle6 = $("#e6").text();
+  let celle7 = $("#e7").text();
+
+  let cellf1 = $("#f1").text();
+  let cellf2 = $("#f2").text();
+  let cellf3 = $("#f3").text();
+  let cellf4 = $("#f4").text();
+  let cellf5 = $("#f5").text();
+  let cellf6 = $("#f6").text();
+  let cellf7 = $("#f7").text();
+
 });
