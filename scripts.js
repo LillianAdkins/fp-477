ff664cfa5879eb0c4ea379694fc65e8c99e47c85
LillianAdkins <ms.lillian.adkins@gmail.com>
2023-04-30 02:34:37 +0000

    Code broke will this work

--- scripts.js
+++ scripts.js
@@ -1,63 +1,74 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
   //variables
   let game = 0;
   let gameRed = 0;
   let gameBlue = 0;
   let draw = 0;
-  let gameStart = true;
-  let startingMark = "red";
+  let red = `<svg class="red" height="80" width="80">
+<circle cx="40" cy="40" r="30" stroke="rgb(191, 64, 41)" stroke-width="3" fill="tomato" />
+</svg>`;
+  let blue = `<svg class="blue" height="80" width="80">
+<circle cx="40" cy="40" r="30" stroke="rgb(84, 124, 198)" stroke-width="3" fill="cornflowerblue" />
+</svg>`;
+  let startingMark = red;
   let turns = 0;
 
-  //boxes
-  //box a
-  let cella1 = $("#a1").text();
-  let cella2 = $("#a2").text();
-  let cella3 = $("#a3").text();
-  let cella4 = $("#a4").text();
-  let cella5 = $("#a5").text();
-  let cella6 = $("#a6").text();
-  let cella7 = $("#a7").text();
+//Turn swapper
+  document.querySelectorAll("td").forEach((cell) =>
+    cell.addEventListener("click", function () {
+      console.log(cell);
+      if (cell.innerHTML == "") {
+        if (startingMark == red) {
+          cell.innerHTML = startingMark;
+          startingMark = blue;
+          document.querySelector("#turn").innerHTML = startingMark;
+          turns++;
+        } else {
+          cell.innerHTML = startingMark;
+          startingMark = red;
+          document.querySelector("#turn").innerHTML = startingMark;
 
-//box b
-  let cellb1 = $("#b1").text();
-  let cellb2 = $("#b2").text();
-  let cellb3 = $("#b3").text();
-  let cellb4 = $("#b4").text();
-  let cellb5 = $("#b5").text();
-  let cellb6 = $("#b6").text();
-  let cellb7 = $("#b7").text();
+          turns++;
+        }
+      } else {
+        alert("You cannot place a token here");
+      }
 
-  let cellc1 = $("#c1").text();
-  let cellc2 = $("#c2").text();
-  let cellc3 = $("#c3").text();
-  let cellc4 = $("#c4").text();
-  let cellc5 = $("#c5").text();
-  let cellc6 = $("#c6").text();
-  let cellc7 = $("#c7").text();  
-  
-  let celld1 = $("#d1").text();
-  let celld2 = $("#d2").text();
-  let celld3 = $("#d3").text();
-  let celld4 = $("#d4").text();
-  let celld5 = $("#d5").text();
-  let celld6 = $("#d6").text();
-  let celld7 = $("#d7").text();
+    })
+  );
 
-  let celle1 = $("#e1").text();
-  let celle2 = $("#e2").text();
-  let celle3 = $("#e3").text();
-  let celle4 = $("#e4").text();
-  let celle5 = $("#e5").text();
-  let celle6 = $("#e6").text();
-  let celle7 = $("#e7").text();
+//Adding functions
+  document.querySelector("#tie").addEventListener("click", function () {
+    draw++
+    document.querySelector("#gameDraws").innerHTML = `Draws: ` + draw;
+  }
+  );
 
-  let cellf1 = $("#f1").text();
-  let cellf2 = $("#f2").text();
-  let cellf3 = $("#f3").text();
-  let cellf4 = $("#f4").text();
-  let cellf5 = $("#f5").text();
-  let cellf6 = $("#f6").text();
-  let cellf7 = $("#f7").text();
+
+  document.querySelector("#redWin").addEventListener("click", function () {
+    gameRed++
+    document.querySelector("#gameRedWins").innerHTML = `Games red has won: ` + gameRed;
+  }
+  );
+
+  document.querySelector("#blueWin").addEventListener("click", function () {
+    gameBlue++
+    document.querySelector("#gameBlueWins").innerHTML = `Games blue has won: ` + gameBlue;
+  }
+  );
+
+  document.querySelector("#newGame").addEventListener("click", function () {
+    document.querySelectorAll("td").forEach((cell) => {
+      cell.innerHTML = "";
+    }
+    );
+    // Reset Plays
+    turns = 0;
+
+    // Increase game count
+    game++;
+    document.querySelector("#games").innerHTML = game;
+  });
 
 });
